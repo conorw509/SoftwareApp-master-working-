0140fcc3d821db09ca6727a44a0044f85056ceb4
Conor Ward <conorward509@hotmail.com>
2018-11-13 20:27:48 +0000

    email+register working

--- app/src/main/java/com/example/conor/softwareapp/Register.java
+++ app/src/main/java/com/example/conor/softwareapp/Register.java
@@ -1,30 +1,100 @@
 package com.example.conor.softwareapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+
+    private EditText etName;
+    private  EditText etEmail;
+    private  Button bRegister;
+    private EditText etPass;
+    private  TextView view;
+    private FirebaseAuth mAuth;
+    private ProgressDialog PD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-
-        final EditText etName = (EditText) findViewById(R.id.RegName);
-        final EditText etUsrName = (EditText) findViewById(R.id.RegEmail);
-        final EditText etPass = (EditText) findViewById(R.id.RegPword);
-        final Button bRegister = (Button) findViewById(R.id.RegBtn);
-        final TextView view = (TextView) findViewById(R.id.LoginBk);
-
+      // etName = (EditText) findViewById(R.id.RegName);
+       etEmail = (EditText) findViewById(R.id.RegEmail);
+       etPass = (EditText) findViewById(R.id.RegPword);
+       bRegister = (Button) findViewById(R.id.RegBtn);
+       view = (TextView) findViewById(R.id.LoginBk);
 
 
+
+        PD = new ProgressDialog(this);
+        PD.setMessage("Loading...");
+        PD.setCancelable(true);
+        PD.setCanceledOnTouchOutside(false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(Register.this, home.class));
+            finish();
+        }
+
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override            public void onClick(View view) {
+                final String email = etEmail.getText().toString();
+                final String password = etPass.getText().toString();
+               // final String name = etName.getText().toString();
+
+                try {
+                    if (password.length() > 0 && email.length() > 0) {
+                        PD.show();
+                        mAuth.createUserWithEmailAndPassword(email, password)
+
+                                .addOnCompleteListener( Register.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (!task.isSuccessful()) {
+                                            Toast.makeText(
+                                                    Register.this,
+                                                    "Authentication Failed",
+                                                    Toast.LENGTH_LONG).show();
+                                            Log.v("error", task.getResult().toString());
+                                        } else {
+                                            Intent intent = new Intent(Register.this, home.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                        PD.dismiss();
+                                    }
+                                });
+                    } else {
+                        Toast.makeText(
+                                Register.this,
+                                "Fill All Fields",
+                                Toast.LENGTH_LONG).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+//back to login
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,9 +102,6 @@ public class Register extends AppCompatActivity {
                 Register.this.startActivity(intent);
             }
         });
-
-
-
 
 
 
