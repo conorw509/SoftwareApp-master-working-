f4d2ddd0071486062feff11511dfc16a756b8049
Conor Ward <conorward509@hotmail.com>
2018-11-14 21:48:32 +0000

    seperate

--- app/src/main/java/com/example/conor/softwareapp/Login.java
+++ app/src/main/java/com/example/conor/softwareapp/Login.java
@@ -16,40 +16,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+
 
 public class Login extends AppCompatActivity {
 
-    SignInButton signInButton;
+
     FirebaseAuth mAuth;
-    GoogleApiClient mGoogleApiClient;
-    FirebaseAuth.AuthStateListener mAuthListener;
-    private final static int RC_SIGN_IN = 2;
     private EditText emailField;
     private EditText passField;
     private Button etLogBtn;
     private TextView regLink;
    // private ProgressBar progressBar;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,8 +43,9 @@ public class Login extends AppCompatActivity {
         passField = (EditText) findViewById(R.id.RegPword);
         etLogBtn = (Button) findViewById(R.id.LoginBtn);
         regLink = (TextView) findViewById(R.id.RegisterBtn);
-       // progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+
+        // progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
         mAuth = FirebaseAuth.getInstance();
 
 //test crashanalytics button
@@ -78,25 +62,6 @@ public class Login extends AppCompatActivity {
                 ViewGroup.LayoutParams.WRAP_CONTENT));
         //end of crash analytics
 
-
-//on click of google sign in button
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signIn();
-            }
-        });
-
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-
-                if (firebaseAuth.getCurrentUser() != null) {
-                    startActivity(new Intent(Login.this, home.class));
-                }
-            }
-        };
 
 //on click of email login button
         etLogBtn.setOnClickListener(new View.OnClickListener() {
@@ -130,7 +95,12 @@ public class Login extends AppCompatActivity {
                     @Override //checks status of task thats proceeded
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            startActivity(new Intent(Login.this, home.class));
+                            if (mAuth.getCurrentUser().isEmailVerified()) {
+                                startActivity(new Intent(Login.this, home.class));
+
+                            } else {
+                                Toast.makeText(Login.this, "Please verify your email", Toast.LENGTH_LONG).show();
+                            }
 
 
                         } else {
@@ -142,6 +112,8 @@ public class Login extends AppCompatActivity {
             }
         });
 
+
+
 //on click bringing to register page
         regLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -152,73 +124,5 @@ public class Login extends AppCompatActivity {
 
             }
         });
-        // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-//google authentication
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-                        Toast.makeText(Login.this, "Something went wrong", Toast.LENGTH_SHORT).show();
-
-                    }
-                })
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-    }
-
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-
-                GoogleSignInAccount account = result.getSignInAccount();
-                firebaseAuthWithGoogle(account);
-            } else {
-
-                Toast.makeText(Login.this, "Authorization Failure", Toast.LENGTH_SHORT).show();
-
-            }
-
-        }
-    }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("Tag", "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            // updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("TAG", "signInWithCredential:failure", task.getException());
-                            Toast.makeText(Login.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
-                        }
-
-                        // ...
-                    }
-                });
     }
 }
