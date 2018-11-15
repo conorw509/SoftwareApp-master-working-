4316ca4c49b7f55a85bc932816cd8be84fed68ca
Conor Ward <conorward509@hotmail.com>
2018-11-15 19:42:29 +0000

    Reg Expression check

--- app/src/main/java/com/example/conor/softwareapp/Register.java
+++ app/src/main/java/com/example/conor/softwareapp/Register.java
@@ -29,13 +29,13 @@ import java.util.regex.Pattern;
 public class Register extends AppCompatActivity {
     private static final Pattern PASSWORD_PATTERN =
             Pattern.compile("^" +
-                    //"(?=.*[0-9])" +         //at least 1 digit
-                    //"(?=.*[a-z])" +         //at least 1 lower case letter
-                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                    "(?=.*[0-9])" +         //at least 1 digit
+                    "(?=.*[a-z])" +         //at least 1 lower case letter
+                    "(?=.*[A-Z])" +         //at least 1 upper case letter
                     "(?=.*[a-zA-Z])" +      //any letter
                     "(?=.*[@#$%^&+=])" +    //at least 1 special character
                     "(?=\\S+$)" +           //no white spaces
-                    ".{4,}" +               //at least 4 characters
+                    ".{6,}" +               //at least 6 characters
                     "$");
 
     //private EditText etName;
