20caf3ab775f9215b8b2b8797d2a6cb2114d59ac
Conor Ward <conorward509@hotmail.com>
2019-01-25 17:43:05 +0000

    small changes

--- app/src/main/java/com/example/conor/softwareapp/support.java
+++ app/src/main/java/com/example/conor/softwareapp/support.java
@@ -1,18 +1,21 @@
 package com.example.conor.softwareapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
-import android.view.Gravity;
+
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.*;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
+
+import java.net.URI;
 
 public class support extends AppCompatActivity {
 
@@ -26,10 +29,25 @@ public class support extends AppCompatActivity {
        final Button signOut = (Button)  findViewById(R.id.LogOutSupport);
        final Button backToHome = (Button)  findViewById(R.id.BackToHomeSupport);
        final TextView txtView = (TextView) findViewById(R.id.row1);
+       final TextView num1 = (TextView) findViewById(R.id.row2);
+
+
 
       txtView.setMovementMethod(LinkMovementMethod.getInstance());
 
         mAuth = FirebaseAuth.getInstance();
+
+        num1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("0857827701"));
+                startActivity(callIntent);
+
+            }
+        });
 
 
         signOut.setOnClickListener(new View.OnClickListener() {
