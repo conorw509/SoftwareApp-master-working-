d45bfa0c4fae017e0def2bb13a079d37bbdb906d
Conor Ward <conorward509@hotmail.com>
2019-01-25 19:59:45 +0000

    small changes

--- app/src/main/java/com/example/conor/softwareapp/support.java
+++ app/src/main/java/com/example/conor/softwareapp/support.java
@@ -1,15 +1,25 @@
 package com.example.conor.softwareapp;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 
 import android.view.View;
 import android.widget.Button;
-import android.widget.*;
+
+import static android.Manifest.permission.CALL_PHONE;
+
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -19,21 +29,20 @@ import java.net.URI;
 
 public class support extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
+    private FirebaseAuth mAuth;
+    private int callPermissionCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.support);
 
-       final Button signOut = (Button)  findViewById(R.id.LogOutSupport);
-       final Button backToHome = (Button)  findViewById(R.id.BackToHomeSupport);
-       final TextView txtView = (TextView) findViewById(R.id.row1);
-       final TextView num1 = (TextView) findViewById(R.id.row2);
+        final Button signOut = (Button) findViewById(R.id.LogOutSupport);
+        final Button backToHome = (Button) findViewById(R.id.BackToHomeSupport);
+        final TextView txtView = (TextView) findViewById(R.id.row1);
+        final TextView num1 = (TextView) findViewById(R.id.row2);
 
-
-
-      txtView.setMovementMethod(LinkMovementMethod.getInstance());
+        txtView.setMovementMethod(LinkMovementMethod.getInstance());
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -41,10 +50,21 @@ public class support extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+               /* if (ContextCompat.checkSelfPermission(support.this,
+                        Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
 
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("0857827701"));
-                startActivity(callIntent);
+                    Toast.makeText(support.this, "Permission to use call feature is already granted",
+                            Toast.LENGTH_LONG).show();
+                    //Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    //callIntent.setData(Uri.parse("0857827701"));
+                    //startActivity(callIntent);
+
+
+                } else {
+
+                    requestCallpermission();
+                }*/
+
 
             }
         });
@@ -72,5 +92,65 @@ public class support extends AppCompatActivity {
 
             }
         });
+
+
+    }
+/*
+    private void requestCallpermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
+            new AlertDialog.Builder(this).setTitle("Permission Needed").setMessage("Permission is needed to make the call")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            ActivityCompat.requestPermissions(support.this, new String[]{Manifest.permission.CALL_PHONE}, callPermissionCode);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, callPermissionCode);
+        }
+    }*/
+
+
+    private void makePhoneCall() {
+
+        if (ContextCompat.checkSelfPermission(support.this,
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(support.this,
+                        new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+            } else {
+                String dial = "tel:" + number;
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+            }
+
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == callPermissionCode) {
+
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Permission Granted",Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(this,"Permission Denied",Toast.LENGTH_LONG).show();
+            }
+
+
+        }
+
+
     }
 }
