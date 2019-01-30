45d16561f45a0c49a86712e850eda4e735670398
Conor Ward <conorward509@hotmail.com>
2019-01-30 14:18:54 +0000

    Logout audio

--- app/src/main/java/com/example/conor/softwareapp/audio.java
+++ app/src/main/java/com/example/conor/softwareapp/audio.java
@@ -7,11 +7,43 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class audio extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.audio);
+
+        final Button signOut = (Button) findViewById(R.id.LogOutAudio);
+        final Button backToHome = (Button) findViewById(R.id.BackToHomeaudio);
+
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent logOutIntent = new Intent(audio.this, LoginInHome.class);
+                audio.this.startActivity(logOutIntent);
+                finish();
+
+            }
+        });
+
+
+        backToHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent logOutIntent = new Intent(audio.this, home.class);
+                audio.this.startActivity(logOutIntent);
+                finish();
+
+            }
+        });
     }
 }
