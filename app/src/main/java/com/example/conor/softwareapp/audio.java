80d6f1baa161505b2288d868a48c8ad3d2665caf
Conor Ward <conorward509@hotmail.com>
2019-01-31 14:25:59 +0000

    song playing from firebase db must retweek for stop next and prev

--- app/src/main/java/com/example/conor/softwareapp/audio.java
+++ app/src/main/java/com/example/conor/softwareapp/audio.java
@@ -11,6 +11,7 @@ import android.os.storage.StorageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,51 +22,54 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class audio extends AppCompatActivity {
 
-
-
-
     private ListView listView;
-    private Button pre,play,next,stop;
+    private Button pre, play, next, stop;
     private MediaPlayer mediaPlayer;
     private SeekBar seekBar;
+    private Button signOut;
+    private Button backToHome;
     private FirebaseAuth mAuth;
-   // private ArrayList<File> list;
-    int cur =0;
-
+    // private ArrayList<File> list;
+    private int cur = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.audio);
-        listView= findViewById(R.id.mainList);
+        signOut = (Button) findViewById(R.id.LogOutAudio);
+        backToHome = (Button) findViewById(R.id.BackToHomeAudio);
+        listView = findViewById(R.id.mainList);
         mAuth = FirebaseAuth.getInstance();
 
         //music objects
-        music music1 = new music("Calm Harp Music","John");
-        music music2 = new music("test","test");
+        music music1 = new music("Calm Harp Music", "John");
+        music music2 = new music("test", "test");
 
         //music list
         ArrayList<music> musicList = new ArrayList<>();
         musicList.add(music1);
         musicList.add(music2);
 
-        musicListAdapter adapter = new musicListAdapter(this,R.layout.custom_listview,musicList);
+        musicListAdapter adapter = new musicListAdapter(this, R.layout.custom_listview, musicList);
         listView.setAdapter(adapter);
 
-       // pre.findViewById(R.id.prev);
+        // pre.findViewById(R.id.prev);
         //play.findViewById(R.id.play);
         //next.findViewById(R.id.next);
         //stop.findViewById(R.id.stop);
         //seekBar.findViewById(R.id.seekBar);
 
-        final Button signOut = (Button) findViewById(R.id.LogOutAudio);
-        final Button backToHome = (Button) findViewById(R.id.BackToHomeAudio);
-
+     listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+         @Override
+         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+             playSong(view);
+         }
+     });
 
         //test button dont think its working
         signOut.setOnClickListener(new View.OnClickListener() {
@@ -92,6 +96,26 @@ public class audio extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    public void playSong(View v) {
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+
+        try {
+            mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/softwareappworkplz.appspot.com/o/Calming-harp-music.mp3?alt=media&token=a0d99b45-a1d0-487d-8b9f-3076afb01724");
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
 
     }
 
