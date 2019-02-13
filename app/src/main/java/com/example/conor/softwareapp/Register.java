cc88078714856a15969097e33ef63b2c477ba870
Conor Ward <conorward509@hotmail.com>
2019-02-13 18:26:59 +0000

    email verification check working
    just need username validation check for reg

--- app/src/main/java/com/example/conor/softwareapp/Register.java
+++ app/src/main/java/com/example/conor/softwareapp/Register.java
@@ -4,20 +4,22 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.ProviderQueryResult;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.HashMap;
 import java.util.regex.Pattern;
 
@@ -37,9 +39,9 @@ public class Register extends AppCompatActivity {
     private Button bRegister;
     private TextView view;
     private FirebaseAuth mAuth;
-    private String email, password, user;
+    private String email, password, user, fireBaseEmail;
     private DatabaseReference reference;
-    private  FirebaseDatabase database;
+    private FirebaseDatabase database;
     private FirebaseUser firebaseUser;
 
     @Override
@@ -55,7 +57,6 @@ public class Register extends AppCompatActivity {
         userName = (EditText) findViewById(R.id.userName);
         view = (TextView) findViewById(R.id.LoginBk);
 
-
         //back to login button
         view.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,13 +70,9 @@ public class Register extends AppCompatActivity {
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 email = etEmail.getText().toString().trim();
                 password = etPass.getText().toString().trim();
                 user = userName.getText().toString().trim();
-
-              //  reference.child("Users").child("userName").equalTo(user);
-
                 if (email.isEmpty() && password.isEmpty() && user.isEmpty()) {
                     Toast.makeText(Register.this, "Fields are empty", Toast.LENGTH_LONG).show();
                     return;
@@ -85,7 +82,6 @@ public class Register extends AppCompatActivity {
                         etEmail.requestFocus();
                         return;
                     }
-
                     if (password.isEmpty()) {
                         etPass.setError("Please enter a Password");
                         etPass.requestFocus();
@@ -107,21 +103,20 @@ public class Register extends AppCompatActivity {
                     etEmail.requestFocus();
                     return;
                 } else {
-                    registerUser(user,email, password);
+                    registerUser(user, email, password);
                 }
             }
         });
     }
 
-    private void registerUser(final String userName,String email, String password) {
-
-
+    private void registerUser(final String userName, final String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             firebaseUser = mAuth.getCurrentUser();
+                            fireBaseEmail = firebaseUser.getEmail();
                             final String userId = firebaseUser.getUid();
                             reference = database.getReference("Users").child(userId);
                             HashMap<String, String> hashMap = new HashMap<>();
@@ -129,7 +124,6 @@ public class Register extends AppCompatActivity {
                             hashMap.put("userName", userName);
                             hashMap.put("imageUrl", "default");
                             reference.setValue(hashMap);
-
 
                             firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
@@ -140,75 +134,37 @@ public class Register extends AppCompatActivity {
                                         Toast.makeText(Register.this, "Please check your email for verification", Toast.LENGTH_LONG).show();
 
                                     } else {
-                                        Toast.makeText(Register.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
-
+                                        Toast.makeText(Register.this, "Unable", Toast.LENGTH_LONG).show();
                                     }
                                 }
                             });
                         } else {
-                            Toast.makeText(Register.this, "Registration failed,please try again", Toast.LENGTH_LONG).show();
-
+                            if (!task.isSuccessful()) {
+                                checkEmail();
+                            } else {
+                                Toast.makeText(Register.this, "Registration failed,please try again", Toast.LENGTH_LONG).show();
+                            }
                         }
                     }
                 });
     }
 
+    public void checkEmail() {
+        mAuth.fetchProvidersForEmail(etEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<ProviderQueryResult>() {
+            @Override
+            public void onComplete(@NonNull Task<ProviderQueryResult> task) {
+
+                boolean check = !task.getResult().getProviders().isEmpty();
+                if (check) {
+
+                    Toast.makeText(Register.this, "Email Already In use Please use a different email", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        mAuth.createUserWithEmailAndPassword(email, password)
-//                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        Log.d("TAG", "createUserWithEmail:onComplete:" + task.isSuccessful());
-//                        if (task.isSuccessful()) {
-//
-//                            //firebaseUser = mAuth.getCurrentUser();
-////                            final String userId = firebaseUser.getUid();
-////                            reference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
-////                            HashMap<String, String> hashMap = new HashMap<>();
-////                            hashMap.put("id", userId);
-////                            hashMap.put("userName", userName);
-////                            hashMap.put("imageUrl", "default");
-//
-//                          //  reference.setValue(hashMap);//.addOnCompleteListener(new OnCompleteListener<Void>() {
-//                                //@Override
-//                              //  public void onComplete(@NonNull Task<Void> task) {
-//                                  //  if (task.isSuccessful()) {
-//                                        mAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
-//                                            @Override
-//                                            public void onComplete(@NonNull Task<Void> task) {
-//                                                if (task.isSuccessful()) {
-//                                                    Toast.makeText(Register.this, "Please check your email for verification", Toast.LENGTH_LONG).show();
-//
-//                                                } else {
-//                                                    Toast.makeText(Register.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
-//                                                }
-//                                            }
-//                                        });
-//                                    //}
-//                               // }
-//                          //  });
-//
-//                        } else {
-//                            Toast.makeText(Register.this, "Registration failed,please try again", Toast.LENGTH_LONG).show();
-//
-//                        }
-//                    }
-//                });
-//    }
+    }
 }
 
 
