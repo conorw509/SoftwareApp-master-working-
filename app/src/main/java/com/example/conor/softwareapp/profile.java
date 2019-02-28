62882a80dd62b789d71e118809b2731cabe22e68
Conor Ward <conorward509@hotmail.com>
2019-02-28 21:08:42 +0000

    design + profile

--- app/src/main/java/com/example/conor/softwareapp/profile.java
+++ app/src/main/java/com/example/conor/softwareapp/profile.java
@@ -4,28 +4,62 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class profile extends AppCompatActivity {
+
     private android.support.v7.widget.Toolbar toolbar;
+    private FirebaseAuth mAuth;
+    private DatabaseReference reference;
+    private FirebaseUser firebaseUser;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_layout);
 
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
         toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolProf);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Profile");
 
-toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        profile.super.onBackPressed();
-    }
-});
+                profile.super.onBackPressed();
+            }
+        });
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    user = snapshot.getValue(User.class);
+                    if (firebaseUser.getUid().equals(user.getId())) {
+                        ((TextView) findViewById(R.id.userP)).setText(user.getUserName());
+                        ((TextView) findViewById(R.id.emailP)).setText(firebaseUser.getEmail());
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
