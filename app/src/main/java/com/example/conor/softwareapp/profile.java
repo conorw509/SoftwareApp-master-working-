c2f345b5b8a31bf50ee76f045b37d241dd1e3119
Conor Ward <conorward509@hotmail.com>
2019-02-28 21:30:40 +0000

    more design

--- app/src/main/java/com/example/conor/softwareapp/profile.java
+++ app/src/main/java/com/example/conor/softwareapp/profile.java
@@ -1,11 +1,9 @@
 package com.example.conor.softwareapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -39,7 +37,6 @@ public class profile extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 profile.super.onBackPressed();
             }
         });
