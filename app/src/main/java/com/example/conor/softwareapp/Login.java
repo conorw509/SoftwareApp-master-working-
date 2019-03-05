5a01a045e870332b5b54ad8dc9081c9ae3aa0df3
Conor Ward <conorward509@hotmail.com>
2019-03-05 13:24:27 +0000

    forgot password

--- app/src/main/java/com/example/conor/softwareapp/Login.java
+++ app/src/main/java/com/example/conor/softwareapp/Login.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -22,8 +23,8 @@ public class Login extends AppCompatActivity {
     private EditText emailField;
     private EditText passField;
     private Button etLogBtn;
-    private  Button backBtn;
-    private TextView regLink,userN,emailN;
+    private Button backBtn;
+    private TextView regLink, forgotP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +35,18 @@ public class Login extends AppCompatActivity {
         passField = (EditText) findViewById(R.id.RegPword);
         etLogBtn = (Button) findViewById(R.id.LoginBtn);
         regLink = (TextView) findViewById(R.id.RegisterBtn);
-        backBtn =(Button) findViewById(R.id.BackToLoginBtn);
+        forgotP = (TextView) findViewById(R.id.forgotPwod);
+        backBtn = (Button) findViewById(R.id.BackToLoginBtn);
         mAuth = FirebaseAuth.getInstance();
+
+        forgotP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,resetPword.class));
+
+            }
+        });
+
 
         etLogBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,18 +78,15 @@ public class Login extends AppCompatActivity {
 
                             if (mAuth.getCurrentUser().isEmailVerified()) {
 
-                                    Intent journalIntent = new Intent(Login.this, home.class);
-                                    Login.this.startActivity(journalIntent);
-                                    finish();
+                                Intent journalIntent = new Intent(Login.this, home.class);
+                                Login.this.startActivity(journalIntent);
+                                finish();
 
-                            }
-
-                            else {
+                            } else {
                                 Toast.makeText(Login.this, "Please verify your email", Toast.LENGTH_LONG).show();
                             }
 
-                        }
-                        else if (!task.isSuccessful()) {
+                        } else if (!task.isSuccessful()) {
                             Toast.makeText(Login.this, "Sign in Problem", Toast.LENGTH_LONG).show();
                         }
                     }
---
+++ app/src/main/java/com/example/conor/softwareapp/resetPword.java
@@ -0,0 +1,64 @@
+package com.example.conor.softwareapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class resetPword extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private EditText sendEmail;
+    private Button btn_reset;
+    private FirebaseAuth mAuth;
+    private String email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reset_password);
+
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        sendEmail = (EditText) findViewById(R.id.sendEmail);
+        btn_reset = (Button) findViewById(R.id.resetBtn);
+        mAuth = FirebaseAuth.getInstance();
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Reset Password");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = sendEmail.getText().toString();
+                if (email.isEmpty()) {
+                    sendEmail.setError("Please Enter An Email");
+
+                } else {
+                    mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(resetPword.this, "Please Check Your Email", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(resetPword.this, Login.class));
+                                finish();
+                            } else {
+                                String error = task.getException().getMessage();
+                                Toast.makeText(resetPword.this, error, Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
