95adc893326843fa06bd49879bca6a8277e3bbb3
Conor Ward <conorward509@hotmail.com>
2019-05-02 16:04:38 +0100

    woo

--- app/src/main/java/com/example/conor/softwareapp/notifications/myFirebaseMessaging.java
+++ app/src/main/java/com/example/conor/softwareapp/notifications/myFirebaseMessaging.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
@@ -20,15 +21,13 @@ public class myFirebaseMessaging extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-
+        String user = remoteMessage.getData().get("user");
+        SharedPreferences preferences = getSharedPreferences("PREFS", MODE_PRIVATE);
+        String currentUser = preferences.getString("currentuser", "none");
         String sented = remoteMessage.getData().get("sented");
-//        String user = remoteMessage.getData().get("user");
-//        SharedPreferences preferences = getSharedPreferences("PREFS", MODE_PRIVATE);
-//        String currentUser = preferences.getString("currentuser", "none");
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
-        if (firebaseUser != null && sented.equals(firebaseUser.getUid())) {
-//            if (!currentUser.equals(user)) {
+        if (!currentUser.equals(user)) {
+            if (firebaseUser != null && sented.equals(firebaseUser.getUid())) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     sendOreoNotification(remoteMessage);
                 } else {
@@ -36,8 +35,7 @@ public class myFirebaseMessaging extends FirebaseMessagingService {
                 }
             }
         }
-
-    //}
+    }
 
     private void sendOreoNotification(RemoteMessage remoteMessage) {
         String user = remoteMessage.getData().get("user");
@@ -94,6 +92,5 @@ public class myFirebaseMessaging extends FirebaseMessagingService {
             i = j;
         }
         noti.notify(i, builder.build());
-
     }
 }
