637fdf7226eb7f212099ba5d5d2e3fedb5ec9eef
Conor Ward <conorward509@hotmail.com>
2019-05-06 12:23:14 +0100

    S

--- app/src/androidTest/java/com/example/conor/softwareapp/mainActivties/audioTest.java
+++ app/src/androidTest/java/com/example/conor/softwareapp/mainActivties/audioTest.java
@@ -3,21 +3,15 @@ package com.example.conor.softwareapp.mainActivties;
 import android.app.Instrumentation;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.KeyEvent;
 import android.widget.ListView;
 import com.example.conor.softwareapp.R;
-import com.example.conor.softwareapp.players.musicPlayer;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.pressKey;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -34,7 +28,6 @@ public class audioTest {
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         rule.launchActivity(new Intent());
         ListView listview = (ListView) rule.getActivity().findViewById(R.id.mainList);
-
         assertThat(listview.getCount(), is(15));
     }
 
@@ -44,12 +37,14 @@ public class audioTest {
         Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
         rule.launchActivity(new Intent());
         final ListView listview = (ListView) rule.getActivity().findViewById(R.id.mainList);
-
+//        final  MediaPlayer mediaPlayer = new MediaPlayer();
         instrumentation.runOnMainSync(new Runnable() {
             @Override
             public void run() {
                 int position = 0;
                 listview.performItemClick(listview.getChildAt(position), position, listview.getAdapter().getItemId(position));
+//                Espresso.onView(withId(R.id.playBtn)).perform(click());
+//                mediaPlayer.pause();
             }
         });
     }
